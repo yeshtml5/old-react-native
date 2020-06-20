c45bacba04bfe77a0cfabf6518afe01040620885
wanhwi.son <yeshtml5@gmail.com>
2020-06-20 18:52:27 +0900

    이벤트 테스트

--- src/screens/main/index.tsx
+++ src/screens/main/index.tsx
@@ -1,36 +1,60 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useEffect} from 'react';
+import {Button, View, Text, Image, StyleSheet, Alert} from 'react-native';
 //style
-
+import Color from 'contexts/color.ts';
 //type
 export interface Props {}
 
 const Main: React.FC<Props> = () => {
+  //--------------------------------------------------------
+  const buttonEventHdr = event => {
+    alert('onClick');
+  };
+  //--------------------------------------------------------
   return (
     <View style={styles.container}>
       {/* 타이틀 */}
       <Text style={styles.title}>Screen Main Title</Text>
       {/* 이미지 */}
       <Image style={styles.image} source={require('assets/images/img1.jpg')} />
+      {/* 버튼 */}
+      <View style={styles.button}>
+        <Button
+          title="Left button"
+          onPress={event => {
+            buttonEventHdr(event);
+          }}
+        />
+      </View>
+      {/* 이미지 */}
+      <Image style={styles.image} source={require('assets/images/img1.jpg')} />
+      {/* 이미지 */}
+      <Image style={styles.image} source={require('assets/images/img1.jpg')} />
     </View>
   );
 };
 export default Main;
-//--------------
+//--------------------------------------------------------
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
     flexDirection: 'column',
   },
   image: {
+    marginBottom: 20,
     height: 400,
     alignItems: 'stretch',
     resizeMode: 'contain', //optional
   },
   title: {
     fontSize: 20,
+  },
+  button: {
+    width: 200,
+    padding: 10,
+    color: Color.red,
+    backgroundColor: Color.black,
   },
 });
