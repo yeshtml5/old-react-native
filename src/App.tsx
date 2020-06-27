f577f0d307102d27d2ddebfc523b43b421834963
wanhwi.son <yeshtml5@gmail.com>
2020-06-27 18:38:43 +0900

    네비게이션 스택구조이해 https://www.youtube.com/watch?v=a9jSyZXYGn8

--- src/App.tsx
+++ src/App.tsx
@@ -17,55 +17,98 @@ import {
   Button,
   StatusBar,
 } from 'react-native';
+
 //contexts
 import Color from 'contexts/color.ts';
 //screen
 import Main from 'screens/main/index.tsx';
+//navigation
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 //type
 export type Props = {};
-
-const App: React.FC<Props> = () => {
-  //--------------------------------------------------------
+/**
+ *
+ */
+const HomeScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-
-      <SafeAreaView>
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Home111 Screen</Text>
-          <Button
-            title="Go to Details"
-            onPress={() => navigation.navigate('Details')}
-          />
-        </View>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          {/* <Main /> */}
-        </ScrollView>
-      </SafeAreaView>
-    </View>
-  );
-};
-export default App;
-export function DetailsScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Details Screen</Text>
+      <Text>Home Screen</Text>
       <Button
-        title="Go to Details... again"
-        onPress={() => navigation.navigate('Details')}
+        title="Go to details"
+        onPress={() => {
+          navigation.navigate('Details');
+        }}
       />
     </View>
   );
-}
-//--------------------------------------------------------
+};
+/**
+ *
+ */
+const DetatilsScreen = ({navigation}) => {
+  return (
+    <View style={styles.detatils}>
+      <Text>DetatilsScreen</Text>
+      <Button
+        title="Go to details again.. again"
+        onPress={() => {
+          navigation.push('Details');
+        }}
+      />
+      <Button
+        title="Home"
+        onPress={() => {
+          navigation.navigate('Home');
+        }}
+      />
+      <Button
+        title="GoBack"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
+};
+/**
+ *
+ */
+const Stack = createStackNavigator();
+const App: React.FC<Props> = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerStyle: {
+              backgroundColor: '#ff0000',
+            },
+            headerTintColor: '#FFFFFF',
+          }}
+        />
+        <Stack.Screen name="Details" component={DetatilsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: Color.white,
+    flexDirection: 'column',
+  },
+  detatils: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ccc',
     flexDirection: 'column',
   },
   scrollView: {
