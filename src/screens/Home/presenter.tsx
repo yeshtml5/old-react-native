9e2fc1ee5b5f5cf8e30f356e534d08b517337c4f
wanhwi.son <yeshtml5@gmail.com>
2020-06-30 00:54:50 +0900

    아이콘문제해결

--- src/screens/Home/presenter.tsx
+++ src/screens/Home/presenter.tsx
@@ -8,28 +8,35 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 //screens
 import {Home, Temp1, Profile} from '@screens';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function Presenter({navigation}) {
   return (
     <Tab.Navigator
       initialRouteName="Feed"
-      tabBarOptions={{
-        activeTintColor: '#FF0000',
-      }}>
+      activeColor="#e91e63"
+      style={{backgroundColor: 'tomato'}}>
       <Tab.Screen
         name="Feed"
         component={Profile}
         options={{
           tabBarLabel: 'Home',
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
         }}
       />
       <Tab.Screen
         name="Notifications"
-        component={Temp1}
+        component={Profile}
         options={{
-          tabBarLabel: 'Overview',
+          tabBarLabel: 'Updates',
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
         }}
       />
       <Tab.Screen
@@ -37,8 +44,38 @@ export default function Presenter({navigation}) {
         component={Profile}
         options={{
           tabBarLabel: 'Profile',
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
         }}
       />
     </Tab.Navigator>
+    // <Tab.Navigator
+    //   initialRouteName="Feed"
+    //   tabBarOptions={{
+    //     activeTintColor: '#FF0000',
+    //   }}>
+    //   <Tab.Screen
+    //     name="Feed"
+    //     component={Profile}
+    //     options={{
+    //       tabBarLabel: 'Home',
+    //     }}
+    //   />
+    //   <Tab.Screen
+    //     name="Notifications"
+    //     component={Temp1}
+    //     options={{
+    //       tabBarLabel: 'Overview',
+    //     }}
+    //   />
+    //   <Tab.Screen
+    //     name="Profile"
+    //     component={Profile}
+    //     options={{
+    //       tabBarLabel: 'Profile',
+    //     }}
+    //   />
+    // </Tab.Navigator>
   );
 }
